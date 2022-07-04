f06a621c99047363c62d064df89752bb401b0da1
haozhe <hzcloud08@163.com>
2022-07-04 20:51:26 +0800

    first commit

---
+++ src/main/java/com/hehe/git/maince.java
@@ -0,0 +1,7 @@
+package com.hehe.git;
+
+public class maince {
+    public static void main(String[] args) {
+        System.out.println("真的很厉害哦");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<h2>Hellddfvd!</h2>
+<h2>Hefdbfdbld!</h2>
+</body>
+</html>
